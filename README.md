96f231268395db9e7c9f6b286e7b07e86366f2f2
Timo Cingöz <timo.cingoez@prof4.net>
2023-07-30 18:44:28 +0200

    implement backpropagation

--- README.md
+++ README.md
@@ -5,6 +5,4 @@
 # Backpropagation
 * efficiently evaluate the gradient of a loss function with respect to the weights of a NN*
 * this allows to iteratively tune the weights of the NN* to minimize the **loss function** which improves the accuracy of the NN*
-
-
-*NN - neural network
+* basically a recursive application of chain rule backwards through the computation graph
--- test.py
+++ test.py
@@ -1,16 +1,135 @@
 import math
 import numpy as np
 import matplotlib.pyplot as pyplot
+from graphviz import Digraph, Source
+
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+        
+    def __repr__(self):
+        return f"Value(node={self.label})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def tanh(self):
+        x = self.data 
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
 
 def f(x):
     return 3*x**2 - 4*x + 5
 
-n = f(3.0)
-print(n)
 
-xs = np.arange(-5, 5, 0.25)
-print(xs)
-ys = f(xs)
-print(ys)
-pyplot.plot(xs, ys)
-pyplot.show()
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+        if n._op:
+            dot.node(name = uid + n._op, label = n._op)
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
+
+
+def save_and_display_dot(dot, file_path):
+    dot.format = 'svg'
+    dot.render(file_path, view=True)
+
+
+# example #1
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b; e.label = 'e'
+d = e + c; d.label = 'd'
+f = Value(-2.0, label='f')
+L = d*f; L.label = 'L'
+# dot = draw_dot(L)
+# save_and_display_dot(dot, "output_graph")
+
+# example #2 with tanh squash + backward propagation for gradients
+x1 = Value(2.0, label="x1")
+x2 = Value(0.0, label="x2")
+
+w1 = Value(-3.0, label="w1")
+w2 = Value(1.0, label="w2")
+
+b = Value(6.8813735870195432, label="b")
+
+x1w1 = x1*w1; x1w1.label = 'x1*w1'
+x2w2 = x2*w2; x2w2.label = 'x2*w2'
+
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+
+n = x1w1x2w2 + b; n.label = 'n'
+o = n.tanh(); o.label = 'o'
+
+o.backward()
+
+dot = draw_dot(o)
+save_and_display_dot(dot, "output_graph")
